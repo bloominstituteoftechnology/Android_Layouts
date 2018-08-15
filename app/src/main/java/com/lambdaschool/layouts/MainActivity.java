8dd0b0cb8943f651ded99e553e858036df1d2149
Chance Payne <chance@lambdaschool.com>
2018-08-15 17:32:49 -0600

    Add landscape layout

--- app/src/main/java/com/lambdaschool/layouts/MainActivity.java
+++ app/src/main/java/com/lambdaschool/layouts/MainActivity.java
@@ -2,12 +2,29 @@ package com.lambdaschool.layouts;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView image;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         setContentView(R.layout.activity_main);
         super.onCreate(savedInstanceState);
+
+        // Not required
+        image = findViewById(R.id.image_view);
+        image.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (image.getScaleType() == ImageView.ScaleType.FIT_CENTER) {
+                    image.setScaleType(ImageView.ScaleType.CENTER_CROP);
+                } else {
+                    image.setScaleType(ImageView.ScaleType.FIT_CENTER);
+                }
+            }
+        });
     }
 }
